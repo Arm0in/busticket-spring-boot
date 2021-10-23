c609a1eb0a82b12f4aab6c26d213017c46433f38
Armin Barghi <armin.barghi@gmail.com>
2021-10-23 21:36:47 +0330

    init project

---
+++ src/main/java/ir/maktab/busticketspringboot/BusticketSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ir.maktab.busticketspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusticketSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusticketSpringBootApplication.class, args);
+	}
+
+}
